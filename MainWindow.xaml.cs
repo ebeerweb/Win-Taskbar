19526c212ae600f9d88b4531cbb8b22996af9878
Aidan Vitek <ajvitek@syr.edu>
2024-04-13 16:50:13 -0400

    Win cs project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace RegistryToggleApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnableButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecutePowerShellScript("enable_taskbar.ps1");
+        }
+
+        private void DisableButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecutePowerShellScript("disable_taskbar.ps1");
+        }
+
+        private void ExecutePowerShellScript(string scriptName)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "powershell";
+                psi.Arguments = $"-ExecutionPolicy Bypass -File \"{scriptName}\"";
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error executing PowerShell script: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
